9bf1b2fe1a93b24dea125cdd69a87ad5edd8bebc
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2025-01-21 18:26:37 +0200

    added getLatestMovies method

--- CinemaArena.Services.Data/MovieService.cs
+++ CinemaArena.Services.Data/MovieService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CinemaArena.Data;
+using CinemaArena.Data.Models;
+using CinemaArena.Services.Data.Interfaces;
+using CinemaArena.Web.ViewModels.Movie;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,28 @@ using System.Threading.Tasks;
 
 namespace CinemaArena.Services.Data
 {
-	internal class MovieService
+	public class MovieService : IMovieService
 	{
+		private readonly CinemaArenaDbContext context;
+
+		public MovieService(CinemaArenaDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task<MovieViewModel[]> GetFiveLatestMovies()
+		{
+			Movie[] allMovies = await context.Movies.AsNoTracking().OrderBy(x => x.Id).ToArrayAsync();
+
+			MovieViewModel[] movies = allMovies
+				.Select(m => new MovieViewModel
+				{
+					Id = m.Id,
+					Name = m.Title,
+					Image = m.Image
+				}).ToArray();
+
+			return movies;
+		}
 	}
 }
