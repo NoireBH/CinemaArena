711baf1799138c0394e4893ee189817c514a8628
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2025-01-21 17:47:22 +0200

    added owlcarousel2 to the project

---
+++ CinemaArena/wwwroot/js/owlCarouselInit.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        items: 1 // Adjust based on your needs
+    });
+});
